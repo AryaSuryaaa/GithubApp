f8dc595833066b5c629a3087f84c255c90447b70
AryaSuryaa <suryasaputra914@gmail.com>
2023-09-20 22:31:23 +0700

    Add library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("androidx.navigation.safeargs")
+    id ("kotlin-kapt")
 }
 
 android {
@@ -54,10 +55,27 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("androidx.activity:activity-ktx:1.7.2")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
+
+    //testing
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //room
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.room:room-runtime:2.6.0-beta01")
+    kapt("androidx.room:room-compiler:2.5.0-alpha02")
+
+    //retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    //coroutine support
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2") //viewModelScope
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2") //liveData
+    implementation("androidx.room:room-ktx:2.5.2")
 }
